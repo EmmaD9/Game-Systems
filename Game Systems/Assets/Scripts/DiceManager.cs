18c26581f75c4228a1626915dc0b245b44b48418
EmmaD9 <maduprey1@gmail.com>
2025-11-08 12:53:00 -0500

    basic dice movement progress

--- Game Systems/Assets/Scripts/DiceManager.cs
+++ Game Systems/Assets/Scripts/DiceManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,8 +10,19 @@ public class DiceManager : MonoBehaviour
     [SerializeField] private bool physicsDice;
     [SerializeField] private bool rgenDice;
 
+    //[SerializeField] private float diceForce;
+
+    private void Start()
+    {
+        
+    }
     public void RollDice()
     {
-
+        //Debug.Log("applied a force");
+        float diceForce = UnityEngine.Random.Range(-10.0f, 10.0f);
+        Rigidbody rb = Die.GetComponent<Rigidbody>();
+        //rb.AddForce(Vector3.up * diceForce, ForceMode.Impulse);
+        rb.AddForce(Vector3.left * diceForce, ForceMode.Impulse);
+        //rb.AddForce(Vector3.right * diceForce, ForceMode.Impulse);
     }
 }
