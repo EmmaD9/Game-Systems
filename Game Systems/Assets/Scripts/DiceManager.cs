5b1865e13a5380dd36a7159da4d264202dea72c4
EmmaD9 <maduprey1@gmail.com>
2025-11-08 12:19:58 -0500

    progress with dice

---
+++ Game Systems/Assets/Scripts/DiceManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceManager : MonoBehaviour
+{
+    [SerializeField] private GameObject Die;
+
+    [SerializeField] private bool physicsDice;
+    [SerializeField] private bool rgenDice;
+
+    //
+}
